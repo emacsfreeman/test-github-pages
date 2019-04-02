efe80dd7aa21b05b313327ef9e6c504639d56551
Laurent Garnier <emacsfreeman@gmail.com>
2019-04-02 11:28:46 +0200

    Create php2.php

---
+++ php/php2.php
@@ -0,0 +1,24 @@
+Hello world!
+
+With '' symbols:
+<?php
+$first_name = 'Laurent';
+echo 'My first name is ' . $first_name . '.';
+$last_name = 'Garnier';
+echo 'My last name is ' . $last_name . '.';
+
+echo "\n\nWith \"\" symbols:\n";
+echo "My first name is $first_name \n";
+echo "My last name is $last_name \n";
+
+$mark1 = 12;
+echo "The 1st mark is $mark1 \n";
+
+$mark2 = 14;
+echo "The 2d mark is $mark2 \n";
+
+$mean = ($mark1 + $mark2) / 2;
+echo "So, the mean is $mean.";
+?>
+
+Howdy?
