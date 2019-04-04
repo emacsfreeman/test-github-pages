3c2e0b4f8d5291560c25e17ec3aee1c3e86af832
Laurent Garnier <emacsfreeman@gmail.com>
2019-04-04 11:36:49 +0200

    Add files via upload

---
+++ django/django4.py
@@ -0,0 +1,34 @@
+# Lists
+maListe = [1, 2, 3]
+maListe = ['hello', 3.5, True, [1,2,3]]
+print(maListe)
+maListe = ['a', 'b', 'c', 1, 2, 3]
+print(maListe[0])
+print(maListe[-1])
+print(maListe[3:5])
+print("avant")
+print(maListe)
+maListe[0] = "New element"
+print("après")
+print(maListe)
+maListe.append('new item at the end')
+print(maListe)
+maListe.append([4, 5, 6])
+print(maListe)
+maListe.extend([4, 5, 6])
+print(maListe)
+item = maListe.pop()
+print(maListe)
+print(item)
+maListe.reverse()
+print(maListem)
+maListe = [4, 5, 3, 1, 2]
+maListe.sort()
+print(maListe)
+maListe.append(['a', 'b', 'c'])
+print(maListe[5][1])
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+
+first_col = [row[0] for row in matrix]
+
+print(first_col)
