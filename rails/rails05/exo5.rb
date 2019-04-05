4c95d379d0b78a0189ee13f5614292c07b506a0b
Laurent Garnier <emacsfreeman@gmail.com>
2019-04-05 16:24:34 +0200

    Add files via upload

---
+++ rails/rails05/exo5.rb
@@ -0,0 +1,25 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 # inches
+cm = height * 2.54
+weight = 180 # lbs
+kg = weight * 0.4536
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall or #{cm} centimeters."
+puts "He's #{weight} pounds heavy or #{kg} kilograms."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+
+# Ecrire un programme
+# 1) Qui demande la taille en centimètres
+#    pour la convertir en pouces (inches)
+# 2) Qui demande le poids en kilogrammes
+#    pour le convertir en livres (pounds)
