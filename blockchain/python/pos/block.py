24f51f54d99f92d042d958e87be577d265810473
Laurent Garnier <emacsfreeman@gmail.com>
2019-04-07 16:14:24 +0200

    Create block.py

---
+++ blockchain/python/pos/block.py
@@ -0,0 +1,15 @@
+import hashlib
+
+class Block:
+
+    def __init__(self, blockNumber, transactions, timestamp ,signatures):
+        self.blockNumber = blockNumber
+        self.transactions = transactions
+        self.signatures = signatures
+        self.timestamp = timestamp
+
+    def signBlock(self, sign_str):
+        prehashData = sign_str
+        prehash = hashlib.sha3_256(prehashData.encode()).hexdigest().encode()
+        hash_signature = hashlib.sha3_256(prehash).hexdigest()
+        self.signatures.append(hash_signature)
