49440109a0c8528b0d9a6bae639bb3334ab58b7e
Laurent Garnier <emacsfreeman@gmail.com>
2019-04-20 00:31:11 +0200

    Add files via upload

---
+++ php/openclassrooms/menu.php
@@ -0,0 +1,13 @@
+<!-- Le menu -->
+
+<nav>
+  <div class="element_menu">
+    <h3>Nouveau nom du menu</h3>
+    <ul>
+      <li><a href="https://www.php-fig.org/psr/" target="_blank">PHP-FIG</a></li>
+      <li><a href="https://www.php-fig.org/psr/psr-1/" target="_blank">PSR-1</a></li>
+      <li><a href="https://www.php-fig.org/psr/psr-2/" target="_blank">PSR-2</a></li>
+      <li><a href="https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/README.rst" target="_blank">GitHub</a></li>
+    </ul>
+  </div>
+</nav>
---
+++ php/openclassrooms/page2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Mon super site</title>
+  </head>
+  <body>
+
+    <?php include("entete.php"); ?>
+
+    <?php include("menu.php"); ?>
+
+    <!-- Le corps -->
+
+    <div id="corps">
+      <h1>Plus d'infos sur moi</h1>
+
+      <p>
+        Sur cette page, je vais vous parler un peu plus dans les détails de moi. <br>
+        Qui suis-je ? Quelle est ma vie, mon oeuvre ?
+      </p>
+    </div>
+
+    <?php include("pieddepage.php"); ?>
+
+  </body>
+</html>
