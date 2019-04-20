282f62e1f3a50993402f3c3d8080642a769b17ed
Laurent Garnier <emacsfreeman@gmail.com>
2019-04-20 14:24:42 +0200

    add files via upload

---
+++ php/openclassrooms/superglobales.php
@@ -0,0 +1,13 @@
+<pre>
+  <?php
+    print_r($_GET)
+   ?>
+</pre>
+
+<ul>
+  <li>$_SERVER['REMOTE_ADDR'] donne l'adresse IP du client qui a demandé à voir la page</li>
+  <li>$_ENV variables d'environnement</li>
+  <li>$_SESSION variables de session</li>
+  <li>$_COOKIE contient les valeurs des cookies</li>
+  <li>$_FILES elle contient la liste des fichiers qui ont été envoyés via le formulaire</li>
+</ul>
