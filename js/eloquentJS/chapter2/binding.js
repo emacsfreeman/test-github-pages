fca805498dc99cd5c566446e97aa74fa27210c64
Laurent Garnier <emacsfreeman@gmail.com>
2019-04-24 22:50:22 +0200

    Create binding.js

---
+++ js/eloquentJS/chapter2/binding.js
@@ -0,0 +1,13 @@
+let cinq = 5;
+console.log(cinq);
+
+let mood = "light";
+console.log(mood);
+
+mood = "dark";
+console.log(mood);
+
+var name = "Laurent";
+const greeting = "Hello ";
+console.log(greeting + name);
+
