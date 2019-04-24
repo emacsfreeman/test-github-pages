274044f255cf1856587e12c1cf47bbf932bed470
Laurent Garnier <emacsfreeman@gmail.com>
2019-04-24 23:30:10 +0200

    Update controlflow.js

--- js/eloquentJS/chapter2/controlflow.js
+++ js/eloquentJS/chapter2/controlflow.js
@@ -2,4 +2,20 @@ let theNumber = Number(prompt("Pick a number"));
 if (!Number.isNaN(theNumber)) 
 {
   console.log("Your number is the square root of " +theNumber * theNumber);
+  if (theNumber < 10)
+  {
+    console.log("Small");
+  }
+  else if (theNumber < 100)
+  {
+    console.log("Medium");
+  }
+  else
+  {
+    console.log("Large");
+  }
+}
+else
+{
+  console.log("Hey! Why didn't you give me a number?");
 }
