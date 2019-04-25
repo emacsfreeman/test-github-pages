880f114f9bba5c2f1f45360594373b049c13cccb
Laurent Garnier <emacsfreeman@gmail.com>
2019-04-25 09:06:49 +0200

    Update exo3chessBoard.js

--- js/eloquentJS/chapter2/exo3chessBoard.js
+++ js/eloquentJS/chapter2/exo3chessBoard.js
@@ -1,8 +1,29 @@
+// Version 1 : for the chessBoard
 let size = 8;
 for (let x = 0; x < size; x++) 
 {
     let string = "";
     for (let y = 0; y < size; y++) 
+    {
+        if ((x + y) % 2 == 0) 
+        {
+            string += " ";
+        }
+        else if ((x + y) % 2 == 1)
+        {
+            string += "#";
+        }
+    }
+    console.log(string + "\n");
+}
+
+// Version 2 : for any grid
+let length = 6;
+let width = 12;
+for (let x = 0; x < length; x++) 
+{
+    let string = "";
+    for (let y = 0; y < width; y++) 
     {
         if ((x + y) % 2 == 0) 
         {
