3c06337d7c8019b8b9586e793e484d66e9a57982
Laurent Garnier <emacsfreeman@gmail.com>
2019-05-03 18:32:40 +0200

    Add files via upload

---
+++ cpp/severalfiles/Car.cpp
@@ -0,0 +1,27 @@
+//
+//  Car.cpp
+//  SeveralFiles
+//
+//  Created by Laurent Garnier on 03/05/2019.
+//  Copyright © 2019 Laurent Garnier. All rights reserved.
+//
+
+#include <iostream>
+#include "Car.hpp"
+
+using namespace std;
+
+void Car::blowHorn()
+{
+    cout << "BLOWING HORN!";
+}
+
+Car::Car()
+{
+    
+}
+
+Car::~Car()
+{
+    
+}
---
+++ cpp/severalfiles/Car.hpp
@@ -0,0 +1,26 @@
+//
+//  Car.hpp
+//  SeveralFiles
+//
+//  Created by Laurent Garnier on 03/05/2019.
+//  Copyright © 2019 Laurent Garnier. All rights reserved.
+//
+
+#ifndef Car_hpp
+#define Car_hpp
+
+class Car
+{
+public:
+    Car();
+    virtual ~Car();
+    
+    void blowHorn();
+    
+protected:
+    
+    
+private:
+};
+
+#endif /* Car_hpp */
