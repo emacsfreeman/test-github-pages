811473e87818cbb85b99fe267f3fc64e2e21d25a
Laurent Garnier <emacsfreeman@gmail.com>
2019-05-03 18:31:57 +0200

    Create main.cpp

---
+++ cpp/severalfiles/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  SeveralFiles
+//
+//  Created by Laurent Garnier on 03/05/2019.
+//  Copyright © 2019 Laurent Garnier. All rights reserved.
+//
+
+#include <iostream>
+#include "Car.hpp"
+
+using namespace std;
+
+int main()
+{
+    Car c = Car();
+    c.blowHorn();
+    cout << "Hello world!" << endl;
+    return 0;
+}
