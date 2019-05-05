2006f3c7d15b9fdf04188f45cd3b7e30f6a9da34
Laurent Garnier <emacsfreeman@gmail.com>
2019-05-05 10:18:55 +0200

    Create Dog3.sol

---
+++ blockchain/solidity/Dog3.sol
@@ -0,0 +1,24 @@
+pragma solidity 0.5.1;
+
+contract DogContract
+{
+    struct Dog 
+    {
+        string name;
+        uint age;
+    }
+    
+    mapping(address => Dog) ownerToDog;
+    
+    function addDog(string memory _name, uint _age) public  
+    {
+        Dog memory currentDog = Dog(_name, _age);
+        ownerToDog[msg.sender] = currentDog;
+    }
+    
+    function getDog(uint _id) public view returns (string memory)
+    {
+        address owner = msg.sender;
+        return ownerToDog[owner].name;
+    }
+}
