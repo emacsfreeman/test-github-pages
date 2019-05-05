b45b36ba441545982db3533527eaf1f2952adea3
Laurent Garnier <emacsfreeman@gmail.com>
2019-05-05 08:50:22 +0200

    Create Dog.sol

---
+++ blockchain/solidity/Dog.sol
@@ -0,0 +1,22 @@
+pragma solidity 0.5.1;
+
+contract DogContract
+{
+    string output;
+    
+    constructor(string memory _output) public 
+    {
+        output = _output;
+    }
+    
+    function bark() public view returns (string memory)
+    {
+        return output;
+    }
+    
+    function setOutput(string memory _output) public
+    {
+        output = _output;
+    }
+    
+}
