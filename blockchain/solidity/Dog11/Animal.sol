9dcd990c1c17e0f57940a57d8b0efc026de9d5ba
Laurent Garnier <emacsfreeman@gmail.com>
2019-05-06 15:42:47 +0200

    Create Animal.sol

---
+++ blockchain/solidity/Dog11/Animal.sol
@@ -0,0 +1,32 @@
+pragma solidity 0.5.1;
+
+import "./Ownable.sol";
+
+contract AnimalContract is Ownable
+{
+    enum AnimalType {DOG, CAT}
+    
+    struct Animal 
+    {
+        string name;
+        uint age;
+        AnimalType animalType;
+    }
+    
+    
+    mapping(address => Animal[]) ownerToAnimals;
+    
+    
+    // _functionName internal
+    function _addAnimal(string memory _name, uint _age, AnimalType _animalType) internal returns (uint) 
+    {
+        return ownerToAnimals[msg.sender].push(Animal(_name, _age, _animalType)) - 1;
+    }
+    
+    function getAnimal(uint _id) public view returns (string memory)
+    {
+        address owner = msg.sender;
+        return ownerToAnimals[msg.sender][_id].name;
+    }
+    
+}
