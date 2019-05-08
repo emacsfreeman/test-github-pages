53c8728ac2c294a281b36292fa85b2bc591af4cd
Laurent Garnier <emacsfreeman@gmail.com>
2019-05-08 14:27:11 +0200

    Update pitfalls.sol

--- blockchain/solidity/Pitfalls/pitfalls.sol
+++ blockchain/solidity/Pitfalls/pitfalls.sol
@@ -1,5 +1,9 @@
 pragma solidity ^0.4.0;
 
+///////////
+// BAD WAY
+/////////
+
 // RE-ENTRANCY
 contract Fund
 {
@@ -12,3 +16,39 @@ contract Fund
             shares[msg.sender] = 0;
     }
 }
+
+/*
+    DEVELOPMENT PATTERN
+    CHECK-EFFECTS-INTERACTIONS
+*/
+
+////////////
+// GOOD WAY
+///////////
+contract Fund
+{
+    // Mapping of ether shares of the contract
+    mapping(address => uint) shares;
+    // Withdraw your share 
+    function withdraw() public 
+    {
+        var share = shares[msg.sender];
+        shares[msg.sender] = 0;
+        msg.sender.transfer(share);
+    }
+}
+
+
+// BAD PRACTICES
+
+// LOOPS WITHOUT FIXED NUMBER OF ITERATIONS 
+
+
+// NEVER USE TX.ORIGIN FOR AUTHENTIFICATION - USE MSG.SENDER 
+// PEOPLE COULD PROXY TRANSACTIONS AND ACT AS you
+
+
+
+// FAIL SAFE MODE 
+
+// READ THIS https://solidity.readthedocs.io/en/v0.4.0/security-considerations.html#pitfalls
