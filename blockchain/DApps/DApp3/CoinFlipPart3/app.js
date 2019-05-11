a1ef18001c5b6fe6132af922a36ca06b6c0a750c
Laurent Garnier <emacsfreeman@gmail.com>
2019-05-11 15:39:21 +0200

    Create app.js

---
+++ blockchain/DApps/DApp3/CoinFlipPart3/app.js
@@ -0,0 +1,69 @@
+(function (Contract) {
+    var web3;
+    var instance;
+
+    function init(cb) {
+        web3 = new Web3(
+            (window.web3 && window.web3.currentProvider) ||
+            new Web3.providers.HttpProvider(Contract.endpoint));
+
+        var contract_interface = web3.eth.contract(Contract.abi);
+        instance = contract_interface.at(Contract.address);
+        cb();
+    }
+
+    function getBalance() {
+        instance.getBalance(function (error, result) {
+            if(error)
+                alert(error);
+            else {
+                $("#balance").html(result.toString());
+            }
+        });
+    }
+
+    function waitForReceipt(txHash, cb) {
+        web3.eth.getTransactionReceipt(txHash, function(error, receipt){
+            if (error) {
+                alert(error);
+            }
+            else if (receipt != null) {
+                cb(receipt);
+            }
+            else {
+                window.setTimeout(function(){
+                    waitForReceipt(txHash, cb);
+                }, 5000);
+            }
+        });
+
+    }
+
+    function flip() {
+        let val = parseInt($("#bet").val());
+        instance.flip.sendTransaction({from: "0x95c2332b26bb22153a689ae619d81a6c59e0a804", gas: 100000, value: val}, function (error, result){
+            if (error) {
+                alert(error);
+            }
+            else {
+                waitForReceipt(txHash, function(receipt){
+                    if (receipt.status === "0x1") {
+                        alert(JSON.stringify(receipt));
+                    }
+                    else {
+                        alert("Receipt status failed");
+                    }
+                });
+            }
+        });
+    }
+
+    $(document).ready(function () {
+        init(function () {
+            getBalance();
+        });
+        $("#submit").click(function (){
+            flip();
+        });
+    });
+})(Contracts['CoinFlip']);
