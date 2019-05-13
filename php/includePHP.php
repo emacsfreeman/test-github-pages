55f21b39173a23e2b11abfdad8b8bba8972f9640
Laurent Garnier <emacsfreeman@gmail.com>
2019-05-13 11:21:25 +0200

    Create includePHP.php

---
+++ php/includePHP.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+
+    <?php
+      $title = "My Fisrt Post";
+      $author = "Laurent";
+      $wordCount = 400;
+      include "article-header.php";
+
+      include "useful-tools.php";
+      sayHi("Mike");
+      echo $feetInMile;
+
+      ?>
+
+
+
+  </body>
+</html>
