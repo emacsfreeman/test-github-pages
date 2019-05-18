96aac8d535a88f3451c740ff219aede01c092368
Laurent Garnier <emacsfreeman@gmail.com>
2019-05-18 19:10:53 +0200

    Create CoinFlip.sol

---
+++ blockchain/DApps/DApp3/CoinFlipPart4/CoinFlip.sol
@@ -0,0 +1,27 @@
+
+pragma solidity ^0.4.17;
+
+contract CoinFlip {
+    address owner;
+    
+    function CoinFlip() public {
+        owner = msg.sender;
+    }
+    
+    function getBalance() constant public returns (uint){
+        return address(this).balance;
+    }
+
+    function flip() payable public {
+        uint time = block.timestamp;
+        uint bet = msg.value;
+
+        if (time % 2 == 0) {
+            msg.sender.transfer(bet + 2); 
+        }
+    }
+
+    function deposit() payable public {
+        
+    }
+}
