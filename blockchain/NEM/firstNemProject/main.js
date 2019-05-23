0748b3ffd2bef61ce6c69692481bf3b748e57d90
Laurent Garnier <emacsfreeman@gmail.com>
2019-05-23 14:49:52 +0200

    Update main.js

--- blockchain/NEM/firstNemProject/main.js
+++ blockchain/NEM/firstNemProject/main.js
@@ -1,3 +1,5 @@
+// SDK : https://github.com/QuantumMechanics/NEM-sdk#11---installation
+
 let nem = require("nem-sdk").default;
 
 let endpoint = nem.model.objects.create("endpoint")(nem.model.nodes.defaultTestnet, nem.model.nodes.defaultPort);
