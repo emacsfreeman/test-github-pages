6fa5284ed9955ca485278ab49a942e21f70bea9a
Laurent Garnier <emacsfreeman@gmail.com>
2019-05-23 15:47:08 +0200

    Add files via upload

---
+++ blockchain/NEM/firstNemProject/firstNemProject.js
@@ -0,0 +1,11 @@
+// SDK : https://github.com/QuantumMechanics/NEM-sdk#11---installation
+
+let nem = require("nem-sdk").default;
+
+let endpoint = nem.model.objects.create("endpoint")(nem.model.nodes.defaultTestnet, nem.model.nodes.defaultPort);
+
+nem.com.requests.chain.lastBlock(endpoint).then(function(block){
+  console.log(block);
+}, function(err){
+  console.log(err);
+});
