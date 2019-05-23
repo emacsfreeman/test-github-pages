a439cef3ceb331f3c3f72dd6e4d938d8b8eb511c
Laurent Garnier <emacsfreeman@gmail.com>
2019-05-23 15:46:24 +0200

    Create transactions.js

---
+++ blockchain/NEM/firstNemProject/transactions.js
@@ -0,0 +1,17 @@
+// SDK : https://github.com/QuantumMechanics/NEM-sdk#11---installation
+
+let nem = require("nem-sdk").default;
+
+let endpoint = nem.model.objects.create("endpoint")(nem.model.nodes.defaultTestnet, nem.model.nodes.defaultPort);
+
+let common = nem.model.objects.create("common")("password", "privateKey");
+
+let transferTransaction = nem.model.objects.create("transferTransaction")("TDTM7NAX3EZ2KD3O22SBRMLFREYWHPTW2GXTSL5W", 10, "Hello World");
+
+let preperedTransaction = nem.model.transactions.prepare("transferTransaction")(common, transferTransaction, nem.model.network.data.testnet.id);
+
+nem.model.transaction.send(common, preparedTransaction, endpoint).then(function(res){
+  console.log(res);
+}, function(err){
+  console.log(err);
+});
