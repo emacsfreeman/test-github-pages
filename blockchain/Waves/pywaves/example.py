9092500d311c5e1927ac901d36c8869c48274629
Laurent Garnier <emacsfreeman@gmail.com>
2019-05-24 19:59:21 +0200

    Create example.py

---
+++ blockchain/Waves/pywaves/example.py
@@ -0,0 +1,9 @@
+import pywaves as pw
+
+myAddress = pw.Address(privateKey='CtMQWJZqfc7PRzSWiMKaGmWFm4q2VN5fMcYyKDBPDx6S')
+otherAddress = pw.Address('3PNTcNiUzppQXDL9RZrK3BcftbujiFqrAfM')
+myAddress.sendWaves(otherAddress, 10000000)
+myToken = myAddress.issueAsset('Token1', 'My Token', 1000, 0)
+while not myToken.status():
+    pass
+myAddress.sendAsset(otherAddress, myToken, 50)
