06eea172c8bec3300fb160a43ac5023d644e24d7
Laurent Garnier <emacsfreeman@gmail.com>
2019-05-25 16:21:02 +0200

    Create api.js

---
+++ blockchain/node.js/udemy/part29/api.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+ 
+app.get('/blockchain', function (req, res) 
+{
+	res.send(bitcoin);
+});
+
+app.post('/transaction', function(req, res) 
+{
+	console.log(req.body);
+	/* 
+		attention à bien utiliser les accents graves `${}` pour 
+		l'interpolation et non les guillemets simples ''
+	*/
+	res.send(`The amount of the transaction is ${req.body.amount} bitcoins.`);
+});
+
+app.get('/mine', function(req, res) {
+
+});
+ 
+app.listen(3000, function() {
+	console.log('Listening on port 3000...');
+});
