b43bd084c7d3ce8971022637a309c92af7f7859e
Laurent Garnier <emacsfreeman@gmail.com>
2019-05-25 15:07:23 +0200

    Create test.js

---
+++ blockchain/node.js/udemy/part17/test.js
@@ -0,0 +1,28 @@
+const Blockchain = require('./blockchain');
+const bitcoin = new Blockchain();
+
+const previousBlockHash = 'OINANFONFINROGN123R';
+const currentBlockData  = 
+[
+	{
+		amount: 10,
+		sender: 'NEOINOF398H49GNA',
+		recipient: 'F3GG5GINN0NU9888JN'
+	},
+	{
+		amount: 30,
+		sender: 'NOF39NEOI8H49GN',
+		recipient: 'F3GG5GINN0NU9888JN'
+	},
+	{
+		amount: 200,
+		sender: 'N398H4EOINOF9GN',
+		recipient: 'GINN0NF3GG5U9888JN'
+	},
+];
+
+const nonce = 100;
+
+
+
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
