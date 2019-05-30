3c621065c6660972a06546a6a04d706ff41da028
Laurent Garnier <emacsfreeman@gmail.com>
2019-05-30 20:45:10 +0200

    Add transferTransaction, mosaicAttachment  main.js

--- blockchain/NEM/randomGame/main.js
+++ blockchain/NEM/randomGame/main.js
@@ -5,10 +5,10 @@ $(document).ready(function () {
 
     // Create an NIS endpoint object
 	var endpoint = nem.model.objects.create("endpoint")(nem.model.nodes.defaultTestnet, nem.model.nodes.defaultPort);
-	var common = nem.model.objects.create("common")("password", "5d533ad1c22fb6237b0ed3471a7ed41a845119543ca4fb7baf47711c4c2549e8");
+	var common = nem.model.objects.create("common")("1983Nem2#75019;", "7a431b6f6047ce1b44fb1c779f5dec3f97817ed644340dfe379b798103f507fb");
 	var mosaicDefinitions = nem.model.objects.get("mosaicDefinitionMetaDataPair");
 
-	var savedScores = JSON.parse(window.localStorage.getItem('random-game-highscore'));
+	var savedScores = JSON.parse(window.localStorage.getItem('random-game-score'));
 	var scores = {};
 
 	if (savedScores !== null)
@@ -30,9 +30,43 @@ $(document).ready(function () {
 		var address = nem.model.address.clean($('#address').val());
 		var result = $('#result').html();
 		if (!(result => 0)) return alert("You need to roll the dice first!");
-		scores[address] = result;
-		window.localStorage.setItem('random-game-highscore', JSON.stringify(scores));
-		updateScoringTable();
+
+		var transferTransaction = nem.model.objects.get('transferTransaction');
+		transferTransaction.amount = 0;
+		transferTransaction.recipient = address;
+		transferTransaction.message = "NEM Random Game Transaction";
+
+		var mosaicAttachment = nem.model.objects.create('mosaicAttachment')("emacsfreeman", "score", result);
+		transferTransaction.mosaics.push(mosaicAttachment);
+
+		nem.com.requests.namespace.mosaicDefinitions(endpoint, mosaicAttachment.mosaicId.namespaceId).then(function(res){
+			var definition = nem.utils.helpers.searchMosaicDefinitionArray(res.data, ["score"]);
+			var fullName = nem.utils.format.mosaicIdToName(mosaicAttachment.mosaicId);
+			mosaicDefinitions[fullName] = {};
+			mosaicDefinitions[fullName].mosaicDefinition = definition[fullName];
+
+			var preparedTransaction = nem.model.transactions.prepare("mosaicTransferTransaction")(common, transferTransaction, mosaicDefinitions, nem.model.network.data.testnet.id);
+			preparedTransaction.fee = 500000;
+
+			nem.model.transactions.send(common, preparedTransaction, endpoint).then(function(res){
+				if (res.code >= 2)
+				{
+					 alert(res.message);
+				}
+				else
+				{
+					scores[address] = result;
+					window.localStorage.setItem('random-game-score', JSON.stringify(scores));
+					updateScoringTable();
+				}
+			}, function(err){
+				console.log(err);
+			});
+		}, function(err){
+			console.log(err);
+		});
+
+
   }
 
 	function updateScoringTable(){
