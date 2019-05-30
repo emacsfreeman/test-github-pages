44b81ab737caedf3b5a341de5e84023e5604b4ce
Laurent Garnier <emacsfreeman@gmail.com>
2019-05-31 00:22:34 +0200

    Create blockchain.py

---
+++ blockchain/python/pow/blockchain_a_z/blockchain.py
@@ -0,0 +1,55 @@
+################
+## Importations
+###############
+
+import datetime
+# https://docs.python.org/3/library/datetime.html
+
+import hashlib
+# https://docs.python.org/3/library/hashlib.html
+
+import json
+# https://docs.python.org/3/library/json.html
+
+from flask import Flask, jsonify
+# Flask: http://flask.pocoo.org/docs/1.0/latexindex/?highlight=flask
+
+# jsonify: http://flask.pocoo.org/docs/1.0/api/?highlight=jsonify#flask.json.jsonify
+
+
+
+class Blockchain:
+
+  def __init__(self):
+    self.chain = []
+    self.create_block(proof = 1, previous_hash = '0')
+
+  def create_block(self, proof, previous_hash):
+    # définition du dico block
+    block = 
+    { 
+      'index': len(self.chain) + 1, 
+      'timestamp': str(datetime.datetime.now()),
+      'proof': proof,
+      'previous_hash': previous_hash
+    }
+    # ajout du bloc à la chaîne
+    self.chain.append(block)
+    
+    return block
+
+  def get_previous_block(self):
+    return self.chain[-1]
+
+  def proof_of_work(self, previous_proof):
+    new_proof = 1
+    check_proof = False
+    while check_proof is False:
+      hash_operation = hashlib.sha256(str(new_proof**2 + previous_proof**2).encode()).hexdigest()
+      if hash_operation[:4] = '0000':
+        check_proof = True
+      else:
+        new_proof += 1
+    return new_proof
+
+
