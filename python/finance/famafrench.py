0afbafe5cb6ae97e592b5c1e3d67bea834067107
Laurent Garnier <emacsfreeman@gmail.com>
2019-06-01 19:12:27 +0200

    Add source

--- python/finance/famafrench.py
+++ python/finance/famafrench.py
@@ -1,5 +1,8 @@
 # WORKS PERFECTLY!!!!!!!!!
 
+# Source
+# https://pandas-datareader.readthedocs.io/en/latest/remote_data.html
+
 from pandas_datareader.famafrench import get_available_datasets
 import pandas_datareader.data as web
 
