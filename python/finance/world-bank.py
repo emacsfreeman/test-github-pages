c48443600abd9ce4c7c4b714de517aaeb0e896b9
Laurent Garnier <emacsfreeman@gmail.com>
2019-06-01 23:24:34 +0200

    Finish world-bank.py

--- python/finance/world-bank.py
+++ python/finance/world-bank.py
@@ -9,3 +9,18 @@ mathces = wb.search('gdp.*capita.*const')
 dat = wb.download(indicator='NY.GDP.PCAP.KD', country=['US', 'CA', 'MX'], start=2005, end=2008)
 
 print(dat)
+
+dat['NY.GDP.PCAP.KD'].groupby(level=0).mean()
+
+wb.search('cell.*%').iloc[:,:2]
+
+ind = ['NY.GDP.PCAP.KD', 'IT.MOB.COV.ZS']
+dat = wb.download(indicator=ind, country='all', start=2011, end=2011).dropna()
+dat.columns = ['gdp', 'cellphone']
+print(dat.tail())
+
+
+import numpy as np
+import statsmodels.formula.api as smf
+mod = smf.ols('cellphone ~ np.log(gdp)', dat).fit()
+print(mod.summary())
