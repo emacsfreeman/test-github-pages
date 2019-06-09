03a2d41446f3dfa1fc58b9d553b3b2fcf39944d7
Laurent Garnier <emacsfreeman@gmail.com>
2019-06-09 12:53:03 +0300

    Create multicolor-pentagon.py

---
+++ python/turtle/multicolor-pentagon.py
@@ -0,0 +1,24 @@
+from turtle import *
+
+color(‘aqua’)
+forward(100)
+left(72)
+
+color(‘blue’)
+forward(100)
+left(72)
+
+color(‘coral’)
+forward(100)
+left(72)
+
+color(‘red’)
+forward(100)
+left(72)
+
+color(‘green’)
+forward(100)
+left(72)
+
+
+bye()
